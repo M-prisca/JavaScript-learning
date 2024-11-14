7d15843c351f92c27eac1e93e8bdc53cad823456
Prisca2005 <priscamasereli@gmail.com>
2024-11-14 06:49:10 +0200

    initial commit

---
+++ toUpperCase.js
@@ -0,0 +1,9 @@
+function jadenSmith(){
+    const str="Hi there.. my name is prisca, i'm a trainee at the gym, Rwanda's most intense software development trainee program.";
+    let splitedStr=str.split(" ");
+    for(let i=0; i<splitedStr.length; i++){
+        splitedStr[i]=splitedStr[i].charAt(0).toUpperCase()+splitedStr[i].slice(1);
+    };
+    console.log(splitedStr.join(" "));
+};
+jadenSmith();
