853f50fb6e7be095ff20d7da56d92c696f594883
Prisca Masereli <priscamasereli@gmail.com>
2025-07-23 11:06:19 +0200

    doc: Add instructions switch it up kata

--- Fundamentals/conditionals/switch statement/switch it upkata.js
+++ Fundamentals/conditionals/switch statement/switch it upkata.js
@@ -1,3 +1,12 @@
+// When provided with a number between 0-9, return it in words.
+//  Note that the input is guaranteed to be within the range of 0-9.
+
+// Input: 1
+
+// Output: "One".
+
+// If your language supports it, try using a switch statement.
+
 function switchItUp(number) {
   switch (number) {
     case 1:
